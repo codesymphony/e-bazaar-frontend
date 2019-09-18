a83d9ebc84064eb037f80a0f669482cbb28c6edc
Aditya Loshali <adityaloshali1@gmail.com>
2019-09-18 17:46:55 +0530

    save up lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', (request: express.Request, response: express.Response) => {
-  response.status(200).send('root route working')
-})
+  response.status(200).send('root route working');
+});
 
 app.listen(5000, () => {
-  console.log('server started on port 5000')
-})
+  console.log('server started on port 5000');
+});
