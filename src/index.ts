c16f472d4c894891cd85386332f8edfdc8150921
Aditya Loshali <adityaloshali1@gmail.com>
2019-09-18 17:55:26 +0530

    graphql yoga setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import { GraphQLServer } from 'graphql-yoga';
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.get('/', (request: express.Request, response: express.Response) => {
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.express.get('/', (request: express.Request, response: express.Response) => {
   response.status(200).send('root route working');
 });
 
-app.listen(5000, () => {
-  console.log('server started on port 5000');
-});
+server.start({
+  playground: '/graphql'
+},
+  () => console.log('Server is running on localhost:4000'))
+
+// app.listen(5000, () => {
+//   console.log('server started on port 5000');
+// });
