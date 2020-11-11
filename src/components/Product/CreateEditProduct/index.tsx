74e58f74de5be72b4a8fe2f8cd14e75473fc667c
Aditya Loshali <adityaloshali1@gmail.com>
2020-11-11 23:57:24 +0530

    show react calendar

--- src/components/Product/CreateEditProduct/index.tsx
+++ src/components/Product/CreateEditProduct/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import ReactCalendar from '@components/common/React-Calendar';
+
 const CreateEditProduct: React.FC = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <div>Product</div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <ReactCalendar
+          onDateChange={timestamp => {
+            console.log(timestamp);
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default CreateEditProduct;
